f5f27cd605c8bcd6ba478957d5ef9192fce352dd
OriLight <61224409+orilights@users.noreply.github.com>
2023-12-16 16:01:32 +0800

    feat: 头部插入代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import simpleHtmlPlugin from 'vite-plugin-simple-html'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 
@@ -16,6 +17,13 @@ export default defineConfig({
       imports: ['vue'],
       dts: true,
       vueTemplate: true,
+    }),
+    simpleHtmlPlugin({
+      inject: {
+        data: {
+          injectHead: process.env.INJECT_HEAD || '',
+        },
+      },
     }),
   ],
 })
