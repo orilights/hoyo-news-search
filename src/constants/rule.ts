dcd3e9a262f9e685b2bf663e16ad115aad9a1b18
OriLight <61224409+orilights@users.noreply.github.com>
2024-12-25 23:17:43 +0800

    chore: 更新分类规则

--- src/constants/rule.ts
+++ src/constants/rule.ts
@@ -70,6 +70,11 @@ export const NEWS_CLASSIFY_RULE: Record<string, Record<string, ClassifyRule>> = 
       include: [],
       exclude: [],
     },
+    活动汇总: {
+      keyword: ['活动汇总'],
+      include: [],
+      exclude: [],
+    },
     前瞻预告: {
       keyword: [/特别节目.*即将开启/, '前瞻直播预告', '前瞻特别节目预告'],
       include: [118577, 118573],
@@ -86,12 +91,12 @@ export const NEWS_CLASSIFY_RULE: Record<string, Record<string, ClassifyRule>> = 
       exclude: [],
     },
     版本更新说明: {
-      keyword: ['更新通知', '更新说明', '更新维护预告'],
+      keyword: ['更新通知', /版本.*更新说明/, '更新维护预告'],
       include: [],
       exclude: [],
     },
     版本专题页: {
-      keyword: ['内容专题页', '先行展示页'],
+      keyword: ['内容专题页', '先行展示页', '版本内容页'],
       include: [],
       exclude: [],
     },
@@ -121,7 +126,7 @@ export const NEWS_CLASSIFY_RULE: Record<string, Record<string, ClassifyRule>> = 
       exclude: [],
     },
     声优小剧场: {
-      keyword: ['声优小剧场'],
+      keyword: ['声优小剧场', '璃月雅集'],
       include: [],
       exclude: [],
     },
@@ -132,6 +137,11 @@ export const NEWS_CLASSIFY_RULE: Record<string, Record<string, ClassifyRule>> = 
     },
     PV短片: {
       keyword: ['PV短片'],
+      include: [],
+      exclude: [],
+    },
+    动画短片: {
+      keyword: ['动画短片'],
       include: [],
       exclude: [],
     },
@@ -171,12 +181,27 @@ export const NEWS_CLASSIFY_RULE: Record<string, Record<string, ClassifyRule>> = 
       exclude: [],
     },
     角色演示: {
-      keyword: ['全新角色展示', '角色演示'],
+      keyword: ['全新角色展示', '角色演示', '角色预告'],
       include: [],
       exclude: [],
     },
     拾枝杂谈: {
       keyword: ['拾枝杂谈'],
+      include: [],
+      exclude: [],
+    },
+    角色趣闻小记: {
+      keyword: ['小记'],
+      include: [],
+      exclude: [],
+    },
+    角色技能演示: {
+      keyword: ['角色技能演示', '角色技能展示'],
+      include: [],
+      exclude: [],
+    },
+    角色介绍: {
+      keyword: ['角色介绍'],
       include: [],
       exclude: [],
     },
@@ -309,13 +334,188 @@ export const NEWS_CLASSIFY_RULE: Record<string, Record<string, ClassifyRule>> = 
     },
   },
   zzz: {
+    版本PV: {
+      keyword: ['版本PV'],
+      include: [],
+      exclude: [],
+    },
+    版本更新说明: {
+      keyword: ['更新说明'],
+      include: [],
+      exclude: [],
+    },
+    版本更新通知: {
+      keyword: [/版本.*更新通知/],
+      include: [],
+      exclude: [],
+    },
+    前瞻情报总览: {
+      keyword: ['情报总览', '回顾长图'],
+      include: [],
+      exclude: [],
+    },
+    前瞻预告: {
+      keyword: ['前瞻特别节目预告'],
+      include: [],
+      exclude: [],
+    },
+    前瞻特别节目: {
+      keyword: ['前瞻特别节目'],
+      include: [],
+      exclude: [],
+    },
+    丽都修葺漫谈: {
+      keyword: ['丽都修葺漫谈'],
+      include: [],
+      exclude: [],
+    },
+    代理人档案: {
+      keyword: ['代理人档案'],
+      include: [],
+      exclude: [],
+    },
+    代理人机制介绍: {
+      keyword: ['代理人机制介绍'],
+      include: [],
+      exclude: [],
+    },
+    代理人采访手记: {
+      keyword: ['采访手记'],
+      include: [],
+      exclude: [],
+    },
+    角色头像: {
+      keyword: ['角色头像'],
+      include: [],
+      exclude: [],
+    },
     角色展示: {
       keyword: ['角色展示'],
       include: [],
       exclude: [],
     },
+    代理人战斗情报: {
+      keyword: ['代理人战斗情报'],
+      include: [],
+      exclude: [],
+    },
     角色PV: {
       keyword: ['角色PV'],
+      include: [],
+      exclude: [],
+    },
+    动画短片: {
+      keyword: ['动画短片'],
+      include: [],
+      exclude: [],
+    },
+    过场动画: {
+      keyword: ['过场动画'],
+      include: [],
+      exclude: [],
+    },
+    PV: {
+      keyword: ['PV'],
+      include: [],
+      exclude: [],
+    },
+    EP上架: {
+      keyword: [/EP.*上架/],
+      include: [],
+      exclude: [],
+    },
+    EP: {
+      keyword: ['EP', 'MV'],
+      include: [],
+      exclude: [],
+    },
+    主线说明: {
+      keyword: [/主线.*开启/],
+      include: [],
+      exclude: [],
+    },
+    独家视界说明: {
+      keyword: ['独家视界开启'],
+      include: [],
+      exclude: [],
+    },
+    代理人秘闻说明: {
+      keyword: ['代理人秘闻开启'],
+      include: [],
+      exclude: [],
+    },
+    生日贺图: {
+      keyword: ['生日快乐'],
+      include: [],
+      exclude: [],
+    },
+    过塑手账: {
+      keyword: ['过塑手账'],
+      include: [],
+      exclude: [],
+    },
+    丽都放大镜: {
+      keyword: ['丽都放大镜'],
+      include: [],
+      exclude: [],
+    },
+    新艾利都探店: {
+      keyword: ['新艾利都探店'],
+      include: [],
+      exclude: [],
+    },
+    六分街街头异闻: {
+      keyword: ['六分街街头异闻'],
+      include: [],
+      exclude: [],
+    },
+    邦布们的说明书: {
+      keyword: ['邦布们的说明书'],
+      include: [],
+      exclude: [],
+    },
+    表情包: {
+      keyword: ['表情包'],
+      include: [],
+      exclude: [],
+    },
+    活动说明: {
+      keyword: ['活动说明'],
+      include: [],
+      exclude: [],
+    },
+    网页活动: {
+      keyword: ['网页活动', 'H5'],
+      include: [],
+      exclude: [],
+    },
+    电玩店上新: {
+      keyword: ['电玩店上新'],
+      include: [],
+      exclude: [],
+    },
+    调频说明: {
+      keyword: ['调频说明'],
+      include: [],
+      exclude: [],
+    },
+    音擎上新: {
+      keyword: ['音擎上新'],
+      include: [],
+      exclude: [],
+    },
+    邦布上新: {
+      keyword: ['卓越搭档'],
+      include: [],
+      exclude: [],
+    },
+    壁纸: {
+      keyword: ['壁纸'],
+      include: [],
+      exclude: [],
+    },
+    周边信息: {
+      keyword: ['新品情报'],
       include: [],
       exclude: [],
     },
